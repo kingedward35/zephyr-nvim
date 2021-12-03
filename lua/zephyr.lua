e3c837760a9cf84ca18d25fcd66247bf2b41ee8e
Verlon Smith <vsmith3113@gmail.com>
2021-12-02 20:10:12 -0700

    Update zephyr.lua

--- lua/zephyr.lua
+++ lua/zephyr.lua
@@ -264,58 +264,22 @@ function zephyr.load_plugin_syntax()
     LspDiagnosticsUnderlineHint = {style="undercurl",sp=zephyr.cyan};
         
     -- Cmp
-    -- CmpDocumentation = { fg = c.fg, bg = c.bg_float };
-    -- CmpDocumentationBorder = { fg = c.border_highlight, bg = c.bg_float };
-        
-    --CmpItemAbbrDeprecated = {fg=zephyr.grey};
-    --CmpItemAbbrMatch = {fg=zephyr.cyan};
-    --CmpItemAbbrMatchFuzzy = {fg=zephyr.cyan};
-    --CmpItemKindVariable = {fg=zephyr.teal}:
-    --CmpItemKindInterface = {fg=zephyr.teal}:
-    --CmpItemKindText = {fg=zephyr.teal}:
-    --CmpItemKindFunction = {fg=zephyr.magenta};
-    --CmpItemKindMethod = {fg=zephyr.magenta};
-    --CmpItemKindKeyword = {fg=zephyr.yellow};
-
-    -- CmpItemAbbr = { fg = zephyr.fg };
+    CmpDocumentation = { fg = zephyr.fg, bg = zephyr.black };
+    CmpDocumentationBorder = { fg = zephyr.bg_highlight, bg = zephyr.black };
+    CmpItemAbbr = { fg = zephyr.fg };
     CmpItemAbbrDeprecated = { fg = zephyr.grey, style = "strikethrough" };
     CmpItemAbbrMatch = { fg = zephyr.blue };
     CmpItemAbbrMatchFuzzy = { fg = zephyr.blue};
 
     -- CmpItemKindDefault = { fg = c.fg_dark, bg = c.none };
     -- CmpItemMenu = { fg = c.comment, bg = c.none };
-        
-        
-        
-        
-        
-        
-        
-
     CmpItemKindVariable = { fg = zephyr.teal };
     CmpItemKindText = { fg = zephyr.teal };
     CmpItemKindFunction = { fg = zephyr.magenta };
     CmpItemKindMethod = { fg = zephyr.magenta };
-        
-        
-        
-        
-        
-
     -- CmpItemKindClass = { fg = c.orange, bg = c.none };
-        
-        
-        
-        
     CmpItemKindInterface = { fg = zephyr.teal };
     CmpItemKindKeyword = { fg = zephyr.yellow };
-        
-        
-        
-        
-        
-        
-
     -- CmpItemKindProperty = { fg = c.green1, bg = c.none };
     -- CmpItemKindField = { fg = c.green1, bg = c.none };
     -- CmpItemKindSnippet = { fg = c.dark5, bg = c.none };
